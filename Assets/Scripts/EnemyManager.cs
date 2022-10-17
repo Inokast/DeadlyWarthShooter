31d5fbeda3a52f5e8385451b3f73cd2e8de85450
ProbablyShadower <111791575+ProbablyShadower@users.noreply.github.com>
2022-10-17 15:12:14 -0400

    Added EnemyManager script
    (havent started on it yet)

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyManager : MonoBehaviour
+{
+    //[SerializeField] private GameObject basicEnemy;
+    //[SerializeField] private GameObject bossEnemy;
+    [SerializeField] public GameObject enemySpawnPoints;
+    [SerializeField] public GameObject bossSpawnPoint;
+    private int waveNum = 0;
+    private int levelNum = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LevelChecker()
+    {
+
+    }
+    
+}
