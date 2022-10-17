499839cfae8a7215b13f9b92fd1a3b02b9e3fc5d
ProbablyShadower <111791575+ProbablyShadower@users.noreply.github.com>
2022-10-17 15:41:46 -0400

    Added functions to EnemyManager and added spawnpoints for the enemies/bosses

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -5,12 +5,16 @@ using UnityEngine.UI;
 
 public class EnemyManager : MonoBehaviour
 {
-    //[SerializeField] private GameObject basicEnemy;
-    //[SerializeField] private GameObject bossEnemy;
+    [SerializeField] private GameObject basicEnemy;
+    [SerializeField] private GameObject bossEnemy;
     [SerializeField] public GameObject enemySpawnPoints;
     [SerializeField] public GameObject bossSpawnPoint;
     private int waveNum = 0;
     private int levelNum = 0;
+    private int numEnemies = 0;
+    public int enemySpeed = 2;
+    public int maxEnemies = 2;
+    public bool waveComplete = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,12 +23,25 @@ public class EnemyManager : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //either changing wavechecker to a coroutine or checking if wavecomplete is true, setting it to false, and starting a new wave
     }
 
     public void LevelChecker()
+    {
+        
+    }
+    public void WaveChecker()
+    {
+        if(numEnemies == 0)
+        {
+            waveComplete = true;
+            waveNum++;
+        }
+    }
+
+    public void EnemySpawn()
     {
 
     }
